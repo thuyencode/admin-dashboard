6c5adffea14718e190b4489ec116ac12995037d8
thuyencode <thuyencode@proton.me>
2023-11-27 09:56:33 +0700

    New Vite plugins to optimize website performance

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import simpleHtmlPlugin from 'vite-plugin-simple-html'
+import { webfontDownload } from 'vite-plugin-webfont-dl'
 
 export default defineConfig({
   base: '/admin-dashboard/',
+  plugins: [webfontDownload(), simpleHtmlPlugin({ minify: true })],
 })
